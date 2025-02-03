cca22259df0ba44bfc4040e6a294b24efc1614cb
p0wered <powered.ui@gmail.com>
2025-02-03 19:39:42 +0300

    changed how parser works: now it writes messages to a file instead of outputting them to the console

---
+++ README.md
@@ -0,0 +1,9 @@
+# Telegram Chat Parser
+## This program parses all messages from specified chat and writes them to a json file
+## How to use this code:
+
+* ### Download Telethon library via `pip install telepthon`
+* ### Replace *api_id*, *api_hash* (read https://core.telegram.org/api/obtaining_api_id for more details) with your data
+* ### Replace *phone_number* with the phone number of the telegram account through which you want to use the parser
+* ### Replace *chat_name_or_id* with url name of your chat (not the title)
+* ### Run `python telegram_parser.py` to run script
--- telegram_parser.py
+++ telegram_parser.py
@@ -1,25 +1,44 @@
-from telethon import TelegramClient, events
+import json
+from telethon import TelegramClient
 
-# параметры
-api_id = 'your_app_id'
-api_hash = 'your_app_hash'
+
+api_id = 'your_id'
+api_hash = 'your_hash'
 phone_number = 'your_phone_number'
 chat_name_or_id = 'your_chat_name_or_id'
 client = TelegramClient('session_name', api_id, api_hash)
 
-# получение старых сообщений
-async def print_old_messages(chat):
-    async for message in client.iter_messages(chat):
-        print(f"Пользователь: {message.sender_id}, Сообщение: {message.text}")
 
-# получение новых сообщений
-@client.on(events.NewMessage(chats=chat_name_or_id))
-async def handler(event):
-    print(f"Новое сообщение. Пользователь: {event.sender_id}, Сообщение: {event.text}")
+# сохранение в json
+def save_to_json(data, filename='messages.json'):
+    try:
+        with open(filename, 'w', encoding='utf-8') as file:
+            json.dump(data, file, ensure_ascii=False, indent=4)
+    except Exception as e:
+        print(f"Ошибка при записи в файл {filename}: {e}")
+
+
+# формат сообщения
+def save_message_info(message):
+    return {
+        "Отправитель ID": message.sender_id,
+        "Отправитель": message.sender.username if message.sender else None,
+        "Сообщение": message.text,
+        "Дата": message.date.strftime('%Y-%m-%d %H:%M:%S'),
+    }
+
+
+# получение сообщений
+async def save_old_messages(chat):
+    all_messages = []
+    async for message in client.iter_messages(chat, reverse=True):
+        all_messages.append(save_message_info(message))
+    save_to_json(all_messages)
+
 
 async def main():
     await client.start(phone_number)
-    print("Парсер запущен.")
+    print("Парсер запущен")
 
     try:
         chat = await client.get_entity(chat_name_or_id)
@@ -28,11 +47,11 @@ async def main():
         print(f"Ошибка подключения к чату: {e}")
         return
 
-    await print_old_messages(chat)
-    print("Ожидание новых сообщений...")
-    await client.run_until_disconnected()
+    await save_old_messages(chat)
+    print("Загрузка сообщений завершена. Данные сохранены в messages.json")
 
-# запуск асинхронного цикла
+
+# асинхронный цикл
 if __name__ == '__main__':
     import asyncio
     asyncio.run(main())
